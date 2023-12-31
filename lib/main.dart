16cbdfb70d7531d2b4e106e20073dc2bab65ee8f
obehi234 <obehi.ataga234@gmail.com>
2023-12-31 11:21:51 +0100

    Added padded and centered images in row

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
-    MaterialApp(
+  return runApp(MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: const Center(
-              child: Text('Dicee',
-              style: TextStyle(
-                  color: Colors.white
-              ))),
-          backgroundColor: Colors.red,
-        ) ,
-        body: DicePage(),
-      )
-    )
-  );
+    backgroundColor: Colors.red,
+    appBar: AppBar(
+      title: const Center(
+          child: Text('Dicee', style: TextStyle(color: Colors.white))),
+      backgroundColor: Colors.red,
+    ),
+    body: DicePage(),
+  )));
 }
 
 class DicePage extends StatelessWidget {
@@ -24,11 +18,21 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Row(
-      children:[
-
-      ]
-
+    return Center(
+      child: Row(children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('images/dice1.png'),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('images/dice1.png'),
+          ),
+        ),
+      ]),
     );
   }
 }
